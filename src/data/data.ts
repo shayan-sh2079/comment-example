13e8798d22d6e51fa797c386c4162634a5859304
shayan2079 <shayan.sh20079@gmail.com>
2023-07-15 05:28:22 +0330

    feat: Added mock data

---
+++ src/data/data.ts
@@ -0,0 +1,146 @@
+export const data = [
+    {
+        id: 0,
+        comment: "COMMENT0",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 1,
+        comment: "COMMENT1",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 2,
+        comment: "COMMENT2",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 3,
+        comment: "COMMENT3",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 4,
+        comment: "COMMENT4",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 5,
+        comment: "COMMENT5",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 6,
+        comment: "COMMENT6",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 7,
+        comment: "COMMENT7",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 8,
+        comment: "COMMENT8",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 9,
+        comment: "COMMENT9",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 10,
+        comment: "COMMENT10",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+    {
+        id: 11,
+        comment: "COMMENT11",
+        replies: [{
+            id: 0,
+            comment: "EEEEEEEEEEEEEEEEEEEEEEEE",
+        },{
+            id: 1,
+            comment: "GGGGGGGGGGGGGGGGGGGGGGGGG",
+        }
+        ]
+    },
+]
