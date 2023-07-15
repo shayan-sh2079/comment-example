93f2046b992feb13be70be165e83c37111e6f735
shayan2079 <shayan.sh20079@gmail.com>
2023-07-15 05:29:55 +0330

    feat: Added store provider and mui baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CommentsSection from "./components/CommentsSection";
+import { Provider } from 'react-redux';
+import {store} from "./store/store";
+import {Container, CssBaseline} from "@mui/material";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+        <CssBaseline />
+        <Container maxWidth={"lg"}>
+            <CommentsSection />
+        </Container>
+    </Provider >
   );
 }
 
